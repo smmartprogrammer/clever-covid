313119277e9cc91615dd3b2388b4e9cf7c7f270a
Raghunathji <partabrajp@gmail.com>
2021-02-21 15:17:53 +0500

    uploading into git till table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import "./App.css";
+import InfoBoxs from "./InfoBoxs";
+import Map from "./Map";
+import Table from "./Table";
+import LineGraph from "./LineGraph";
+import { sortData } from "./utils";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  // state = > how to write variable in react
+  // https://disease.sh/v3/covid-19/countries
+  // useEffect = run a piece of code base on a given condition
+  // the code runs inside here will run once
+  // when the component loads and not again
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // United States, turkey
+            value: country.countryInfo.iso2, // UK, US, PK
+          }));
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              Variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBoxs
+            title="Coronavirus cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBoxs
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBoxs
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+        <Map />
+      </div>
+
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live cases by country</h3>
+          <Table countries={tableData} />
+          <h3>world wide new cases</h3>
+          <LineGraph />
+          {/* <Graph /> */}
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/LineGraph.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+
+function LineGraph() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  return <div>{/* <Line data options /> */}</div>;
+}
+
+export default LineGraph;
